58f02f813d98588fc16e0f6fc26dfd1f8619c6b2
Brock Anderson <brock@bandersgeo.ca>
2017-08-01 16:26:44 -0700

    changed dev server port

--- webpack-hot-dev-server.config.js
+++ webpack-hot-dev-server.config.js
@@ -26,7 +26,7 @@ module.exports = require("./make-webpack-config")({
     chunkFilename: "[id].js"
   },
   devServer: {
-    port: 3200,
+    port: 9090,
     path: path.join(__dirname, 'dev-helpers'),
     publicPath: "/",
     noInfo: true,
